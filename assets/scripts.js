a13b197dc5dc28343d68ea919002fcd4c6567552
Rafatuba <rafaolly1@gmail.com>
2022-01-21 13:38:12 -0400

    primeiro commit

---
+++ assets/scripts.js
@@ -0,0 +1,85 @@
+'use strict';
+
+/*
+Respondeu Jesus: 
+"Eu sou o caminho, a verdade e a vida. 
+Ninguém vem ao Pai, a não ser por mim".
+João 14:6
+*/
+
+
+//let banco = [
+ //   {'tarefa': 'Estudar JS', 'status': ''},
+ //   {'tarefa': 'Netflix', 'status': 'checked'}
+//];
+
+
+
+const getBanco = () => JSON.parse(localStorage.getItem ('todoList')) ?? [];
+const setBanco = (banco) => localStorage.setItem ('todoList', JSON.stringify(banco));
+
+const criarItem = (tarefa, status, indice) => {
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = `
+        <input type="checkbox" ${status} data-indice=${indice}>
+        <div>${tarefa}</div>
+        <input type="button" value="X" data-indice=${indice}>
+    `
+    document.getElementById('todoList').appendChild(item);
+}
+
+const limparTarefas = () => {
+    const todoList = document.getElementById('todoList');
+    while (todoList.firstChild) {todoList.removeChild(todoList.lastChild);
+    }   /*para não duplicar se chamar a função atualizarTela() mais de uma vez */
+}
+
+const atualizarTela = () => {
+    limparTarefas();
+    const banco = getBanco();
+    banco.forEach ((item, indice) => criarItem (item.tarefa, item.status, indice));
+}
+
+const inserirItem = (evento) => {
+    const tecla = evento.key;
+    const texto = evento.target.value;
+    if (tecla === 'Enter'){
+        const banco = getBanco();
+        banco.push ({'tarefa': texto, 'status': ''});
+        setBanco(banco);
+        atualizarTela();
+        evento.target.value = '';    /* Limpa a caixa de texto após criar uma tarefa */
+    }
+}
+
+const removerItem = (indice) => {
+    const banco = getBanco();
+    banco.splice (indice, 1);
+    setBanco(banco);
+    atualizarTela();
+}
+
+const atualizarItem = (indice) => {
+    const banco = getBanco();
+    banco[indice].status = banco[indice].status === '' ? 'checked' : '';
+    setBanco(banco);
+    atualizarTela();
+}
+
+const clickItem = (evento) => {
+    const elemento = evento.target;
+    if (elemento.type === 'button') {
+        const indice = elemento.dataset.indice;
+        removerItem(indice);
+    }else if (elemento.type === 'checkbox') {
+        const indice = elemento.dataset.indice;
+        atualizarItem(indice);
+    }
+}
+
+document.getElementById('newItem').addEventListener('keypress', inserirItem);
+document.getElementById('todoList').addEventListener('click', clickItem);
+
+
+atualizarTela();
